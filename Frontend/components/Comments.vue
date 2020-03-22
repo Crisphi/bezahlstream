502d15055e1df34d48986535d85c18e6752d0acf
Tino Prahl <tinoprahl@gmail.com>
2020-03-22 14:46:28 +0100

    Add component  for comments

---
+++ Frontend/components/Comments.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-list two-line avatar>
+    <v-subheader>Kommentare</v-subheader>
+    <v-list-item-group v-model="item" color="primary">
+      <comment v-for="(comment, i) in comments" :key="i" :comment="comment" />
+    </v-list-item-group>
+  </v-list>
+</template>
+
+<script>
+import Comment from '~/components/Comment.vue'
+
+export default {
+  components: {
+    Comment
+  },
+  props: {
+    comments: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
