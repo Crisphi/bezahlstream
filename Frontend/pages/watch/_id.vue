b51f52be64e33cc3b0340ed5fbeb68dcfc80c7e4
Brian W <wusu@meercode.de>
2020-03-23 21:40:24 +0100

    Add dummy comments to video

--- Frontend/pages/watch/_id.vue
+++ Frontend/pages/watch/_id.vue
@@ -8,19 +8,25 @@
         @play="onPlayerPlay($event)"
         @ready="onPlayerReady($event)"
       />
-      <v-container style="margin-top: -30px">
-        <h1>Test Video</h1>
-        <p>22,732 Aufrufe • 22. März 2020</p>
-      </v-container>
+      <v-layout justify-center align-center>
+        <v-container style="margin-top: -30px">
+          <h1>Test Video</h1>
+          <p>22,732 Aufrufe • 22. März 2020</p>
+
+          <comments :comments="comments" />
+        </v-container>
+      </v-layout>
     </div>
   </div>
 </template>
 
 <script>
 import CustomVideoJsPlayer from '~/components/CustomVideoJsPlayer.vue'
+import Comments from '~/components/Comments.vue'
 
 export default {
   components: {
+    Comments,
     CustomVideoJsPlayer
   },
   data() {
@@ -36,7 +42,37 @@ export default {
           durationDisplay: false
         }
         // poster: 'https://bezahlstream.bitkomplex.de/video-demo/bbb_sunflower_1080p_30fps_normal/playlist.jpg'
-      }
+      },
+      comments: [
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+          title: 'Brunch this weekend?',
+          subtitle:
+            "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+        },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+          title: 'Summer BBQ 4',
+          subtitle: "Wish I could come, but I'm out of town this weekend."
+        },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+          title: 'Oui oui',
+          subtitle: 'Do you have Paris recommendations? Have you ever been?'
+        },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+          title: 'Birthday gift',
+          subtitle:
+            'Have any ideas about what we should get Heidi for her birthday?'
+        },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+          title: 'Recipe to try',
+          subtitle:
+            'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.'
+        }
+      ]
     }
   },
   computed: {
